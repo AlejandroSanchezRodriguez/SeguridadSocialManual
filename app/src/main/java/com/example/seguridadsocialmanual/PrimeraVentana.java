35b4c6a041389d80b553217d4f2ca527051e560f
Pablo Mérida <pablomerida2001@gmail.com>
2021-01-13 09:29:11 +0100

    a

--- app/src/main/java/com/example/seguridadsocialmanual/PrimeraVentana.java
+++ app/src/main/java/com/example/seguridadsocialmanual/PrimeraVentana.java
@@ -16,13 +16,12 @@ import java.util.ArrayList;
 import Models.Model_eng;
 import Models.Model_es;
 
-public class PrimeraVentana extends AppCompatActivity implements Serializable {
-    Model_es model_es;
-    Model_eng model_eng;
-    ListView lv;
+public class PrimeraVentana extends AppCompatActivity{
+    static Model_es model_es;
+    static Model_eng model_eng;
+    static ListView lv;
     static String lenguage;
-
-    private ArrayList<String> arrayParaListView;
+    private static ArrayList<String> arrayParaListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,27 +31,27 @@ public class PrimeraVentana extends AppCompatActivity implements Serializable {
         model_eng=new Model_eng();
         lv = (ListView) findViewById(R.id.view_listView);
         listindex();
+        setItemClickLv();
+        setLvAdapter();
         //añadir aqui los pasos en si de la base de datos
     }
 
-    public void listindex(){
+    public static void listindex(){
         try {
             arrayParaListView.clear();
-            //lv.setAdapter(null);
         }
         catch (Exception e){}
 
         switch (lenguage){
             case "ESP":
-                arrayParaListView = this.model_es.getListVSpanish();
+                arrayParaListView = model_es.getListVSpanish();
                 break;
             case "ENG":
-                arrayParaListView = this.model_eng.getListVEnglish();
+                arrayParaListView = model_eng.getListVEnglish();
                 break;
         }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayParaListView);
-        lv.setAdapter(adapter);
+    }
+    public void setItemClickLv(){
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             //añadir aqui el evento al pulsar los pasos
@@ -65,5 +64,10 @@ public class PrimeraVentana extends AppCompatActivity implements Serializable {
                 }
             }
         });
+    }
+
+    public void setLvAdapter(){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayParaListView);
+        lv.setAdapter(adapter);
     }
 }
