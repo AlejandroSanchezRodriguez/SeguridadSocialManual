a1cf83d4abcef2adf9cd1dbae731b9e213764b84
Pablo Mérida <pablomerida2001@gmail.com>
2021-01-14 15:47:21 +0100

    añadir videos de youtube a la demo

--- app/src/main/java/com/example/seguridadsocialmanual/DemoMenu.java
+++ app/src/main/java/com/example/seguridadsocialmanual/DemoMenu.java
@@ -2,7 +2,11 @@ package com.example.seguridadsocialmanual;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -31,6 +35,7 @@ public class DemoMenu extends AppCompatActivity {
 
         loaded = true;
         setText();
+        setListeners();
     }
 
     public static void setText(){
@@ -50,5 +55,47 @@ public class DemoMenu extends AppCompatActivity {
         btn2.setText(values[1]);
         btn3.setText(values[2]);
         btn4.setText(values[3]);
+    }
+
+    public void setListeners(){
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playYoutubeVideo("eqWStH--MZ4");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playYoutubeVideo("7H2KL4JhX6Q");
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playYoutubeVideo("dQw4w9WgXcQ");
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playYoutubeVideo("GgQ3qm32vac");
+            }
+        });
+    }
+
+    public void playYoutubeVideo(String id) {
+        Intent appIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + id));
+        Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.youtube.com/watch?v=" + id));
+        try {
+            startActivity(appIntent);
+        } catch (ActivityNotFoundException ex) {
+            startActivity(webIntent);
+        }
     }
 }
